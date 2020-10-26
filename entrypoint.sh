4215125ac1995b32efe90be76b28c4a09cf1a85e
naofumi-fujii <naofumi.fujii@gmail.com>
2020-10-26 22:20:30 +0900

    remove not using

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,148 +2,148 @@
 
 set -o pipefail
 
-# config
-default_semvar_bump=${DEFAULT_BUMP:-minor}
-with_v=${WITH_V:-false}
-release_branches=${RELEASE_BRANCHES:-master}
-custom_tag=${CUSTOM_TAG}
-source=${SOURCE:-.}
-dryrun=${DRY_RUN:-false}
-initial_version=${INITIAL_VERSION:-0.0.0}
-tag_context=${TAG_CONTEXT:-repo}
-suffix=${PRERELEASE_SUFFIX:-beta}
-verbose=${VERBOSE:-true}
-
-cd ${GITHUB_WORKSPACE}/${source}
-
-echo "*** CONFIGURATION ***"
-echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
-echo -e "\tWITH_V: ${with_v}"
-echo -e "\tRELEASE_BRANCHES: ${release_branches}"
-echo -e "\tCUSTOM_TAG: ${custom_tag}"
-echo -e "\tSOURCE: ${source}"
-echo -e "\tDRY_RUN: ${dryrun}"
-echo -e "\tINITIAL_VERSION: ${initial_version}"
-echo -e "\tTAG_CONTEXT: ${tag_context}"
-echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
-echo -e "\tVERBOSE: ${verbose}"
-
-current_branch=$(git rev-parse --abbrev-ref HEAD)
-
-pre_release="true"
-IFS=',' read -ra branch <<< "$release_branches"
-for b in "${branch[@]}"; do
-    echo "Is $b a match for ${current_branch}"
-    if [[ "${current_branch}" =~ $b ]]
-    then
-        pre_release="false"
-    fi
-done
-echo "pre_release = $pre_release"
-
-# fetch tags
-git fetch --tags
-
-# get latest tag that looks like a semver (with or without v)
-case "$tag_context" in
-    *repo*) 
-        tag=$(git for-each-ref --sort=-v:refname --format '%(refname)' | cut -d / -f 3- | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
-        pre_tag=$(git for-each-ref --sort=-v:refname --format '%(refname)' | cut -d / -f 3- | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-$suffix.[0-9]+)?$" | head -n1)
-        ;;
-    *branch*) 
-        tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
-        pre_tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-$suffix.[0-9]+)?$" | head -n1)
-        ;;
-    * ) echo "Unrecognised context"; exit 1;;
-esac
-
-# if there are none, start tags at INITIAL_VERSION which defaults to 0.0.0
-if [ -z "$tag" ]
-then
-    log=$(git log --pretty='%B')
-    tag="$initial_version"
-    pre_tag="$initial_version"
-else
-    log=$(git log $tag..HEAD --pretty='%B')
-fi
-
-# get current commit hash for tag
-tag_commit=$(git rev-list -n 1 $tag)
-
-# get current commit hash
-commit=$(git rev-parse HEAD)
-
-if [ "$tag_commit" == "$commit" ]; then
-    echo "No new commits since previous tag. Skipping..."
-    echo ::set-output name=tag::$tag
-    exit 0
-fi
-
-# echo log if verbose is wanted
-if $verbose
-then
-  echo $log
-fi
-
-case "$log" in
-    *#major* ) new=$(semver -i major $tag); part="major";;
-    *#minor* ) new=$(semver -i minor $tag); part="minor";;
-    *#patch* ) new=$(semver -i patch $tag); part="patch";;
-    * ) 
-        if [ "$default_semvar_bump" == "none" ]; then
-            echo "Default bump was set to none. Skipping..."; exit 0 
-        else 
-            new=$(semver -i "${default_semvar_bump}" $tag); part=$default_semvar_bump 
-        fi 
-        ;;
-esac
-
-if $pre_release
-then
-    # Already a prerelease available, bump it
-    if [[ "$pre_tag" == *"$new"* ]]; then
-        new=$(semver -i prerelease $pre_tag --preid $suffix); part="pre-$part"
-    else
-        new="$new-$suffix.1"; part="pre-$part"
-    fi
-fi
-
-echo $part
-
-# did we get a new tag?
-if [ ! -z "$new" ]
-then
-	# prefix with 'v'
-	if $with_v
-	then
-		new="v$new"
-	fi
-fi
-
-if [ ! -z $custom_tag ]
-then
-    new="$custom_tag"
-fi
-
-if $pre_release
-then
-    echo -e "Bumping tag ${pre_tag}. \n\tNew tag ${new}"
-else
-    echo -e "Bumping tag ${tag}. \n\tNew tag ${new}"
-fi
-
-# set outputs
-echo ::set-output name=new_tag::$new
-echo ::set-output name=part::$part
-
-# use dry run to determine the next tag
-if $dryrun
-then
-    echo ::set-output name=tag::$tag
-    exit 0
-fi 
-
-echo ::set-output name=tag::$new
+# # config
+# default_semvar_bump=${DEFAULT_BUMP:-minor}
+# with_v=${WITH_V:-false}
+# release_branches=${RELEASE_BRANCHES:-master}
+# custom_tag=${CUSTOM_TAG}
+# source=${SOURCE:-.}
+# dryrun=${DRY_RUN:-false}
+# initial_version=${INITIAL_VERSION:-0.0.0}
+# tag_context=${TAG_CONTEXT:-repo}
+# suffix=${PRERELEASE_SUFFIX:-beta}
+# verbose=${VERBOSE:-true}
+# 
+# cd ${GITHUB_WORKSPACE}/${source}
+# 
+# echo "*** CONFIGURATION ***"
+# echo -e "\tDEFAULT_BUMP: ${default_semvar_bump}"
+# echo -e "\tWITH_V: ${with_v}"
+# echo -e "\tRELEASE_BRANCHES: ${release_branches}"
+# echo -e "\tCUSTOM_TAG: ${custom_tag}"
+# echo -e "\tSOURCE: ${source}"
+# echo -e "\tDRY_RUN: ${dryrun}"
+# echo -e "\tINITIAL_VERSION: ${initial_version}"
+# echo -e "\tTAG_CONTEXT: ${tag_context}"
+# echo -e "\tPRERELEASE_SUFFIX: ${suffix}"
+# echo -e "\tVERBOSE: ${verbose}"
+# 
+# current_branch=$(git rev-parse --abbrev-ref HEAD)
+# 
+# pre_release="true"
+# IFS=',' read -ra branch <<< "$release_branches"
+# for b in "${branch[@]}"; do
+#     echo "Is $b a match for ${current_branch}"
+#     if [[ "${current_branch}" =~ $b ]]
+#     then
+#         pre_release="false"
+#     fi
+# done
+# echo "pre_release = $pre_release"
+# 
+# # fetch tags
+# git fetch --tags
+# 
+# # get latest tag that looks like a semver (with or without v)
+# case "$tag_context" in
+#     *repo*) 
+#         tag=$(git for-each-ref --sort=-v:refname --format '%(refname)' | cut -d / -f 3- | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
+#         pre_tag=$(git for-each-ref --sort=-v:refname --format '%(refname)' | cut -d / -f 3- | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-$suffix.[0-9]+)?$" | head -n1)
+#         ;;
+#     *branch*) 
+#         tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+$" | head -n1)
+#         pre_tag=$(git tag --list --merged HEAD --sort=-v:refname | grep -E "^v?[0-9]+.[0-9]+.[0-9]+(-$suffix.[0-9]+)?$" | head -n1)
+#         ;;
+#     * ) echo "Unrecognised context"; exit 1;;
+# esac
+# 
+# # if there are none, start tags at INITIAL_VERSION which defaults to 0.0.0
+# if [ -z "$tag" ]
+# then
+#     log=$(git log --pretty='%B')
+#     tag="$initial_version"
+#     pre_tag="$initial_version"
+# else
+#     log=$(git log $tag..HEAD --pretty='%B')
+# fi
+# 
+# # get current commit hash for tag
+# tag_commit=$(git rev-list -n 1 $tag)
+# 
+# # get current commit hash
+# commit=$(git rev-parse HEAD)
+# 
+# if [ "$tag_commit" == "$commit" ]; then
+#     echo "No new commits since previous tag. Skipping..."
+#     echo ::set-output name=tag::$tag
+#     exit 0
+# fi
+# 
+# # echo log if verbose is wanted
+# if $verbose
+# then
+#   echo $log
+# fi
+# 
+# case "$log" in
+#     *#major* ) new=$(semver -i major $tag); part="major";;
+#     *#minor* ) new=$(semver -i minor $tag); part="minor";;
+#     *#patch* ) new=$(semver -i patch $tag); part="patch";;
+#     * ) 
+#         if [ "$default_semvar_bump" == "none" ]; then
+#             echo "Default bump was set to none. Skipping..."; exit 0 
+#         else 
+#             new=$(semver -i "${default_semvar_bump}" $tag); part=$default_semvar_bump 
+#         fi 
+#         ;;
+# esac
+# 
+# if $pre_release
+# then
+#     # Already a prerelease available, bump it
+#     if [[ "$pre_tag" == *"$new"* ]]; then
+#         new=$(semver -i prerelease $pre_tag --preid $suffix); part="pre-$part"
+#     else
+#         new="$new-$suffix.1"; part="pre-$part"
+#     fi
+# fi
+# 
+# echo $part
+# 
+# # did we get a new tag?
+# if [ ! -z "$new" ]
+# then
+# 	# prefix with 'v'
+# 	if $with_v
+# 	then
+# 		new="v$new"
+# 	fi
+# fi
+# 
+# if [ ! -z $custom_tag ]
+# then
+#     new="$custom_tag"
+# fi
+# 
+# if $pre_release
+# then
+#     echo -e "Bumping tag ${pre_tag}. \n\tNew tag ${new}"
+# else
+#     echo -e "Bumping tag ${tag}. \n\tNew tag ${new}"
+# fi
+# 
+# # set outputs
+# echo ::set-output name=new_tag::$new
+# echo ::set-output name=part::$part
+# 
+# # use dry run to determine the next tag
+# if $dryrun
+# then
+#     echo ::set-output name=tag::$tag
+#     exit 0
+# fi 
+# 
+# echo ::set-output name=tag::$new
 
 # create local git tag
 git tag $new
